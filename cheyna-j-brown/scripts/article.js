363d77767ab52365d1954605b301eddecbb8bd93
J Christie <jchristie1@gmail.com>
2018-07-17 12:58:35 -0700

    Potentially working product?

--- cheyna-j-brown/scripts/article.js
+++ cheyna-j-brown/scripts/article.js
@@ -34,6 +34,13 @@ Article.prototype.toHtml = function() {
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
 // PUT YOUR RESPONSE HERE
+Article.loadAll = articleData => {
+  articleData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
+
+  articleData.forEach(articleObject => Article.all.push(new Article(articleObject)))
+}
+
+// REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
 Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
@@ -52,5 +59,4 @@ Article.fetchAll = () => {
         console.log(error);
       });
   }
-  
-Article.fetchAll();
+}
